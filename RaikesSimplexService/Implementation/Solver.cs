55e67c0c81b82abdd1f1043d441e33b78824670e
Brobin <tobinjbrown@gmail.com>
2014-11-14 15:33:08 -0600

    started part 2

--- RaikesSimplexService/Implementation/Solver.cs
+++ RaikesSimplexService/Implementation/Solver.cs
@@ -14,12 +14,38 @@ namespace RaikesSimplexService.DuckTheSystem
     {
         public Solution Solve(Model model)
         {
+           var newModel = this.AddSlackSurplusVariables(model);
            throw new NotImplementedException();
         }
 
-        public void CreateMatrices()
+        public Model AddSlackSurplusVariables(Model model)
         {
+            var slack = 0;
+            var artificial = 0;
+            foreach(var c in model.Constraints)
+            {
+                switch(c.Relationship)
+                {
+                    case Relationship.GreaterThanOrEquals:
+                        if(c.Value != 0)
+                        {
+                            slack += 1;
+                            artificial += 1;
+                        }
+                        break;
+                    case Relationship.LessThanOrEquals:
+                        slack += 1;
+                        break;
+                }
+            }
+            var usedSlack = 0;
+            var usedArtificial = 0;
+            foreach(var c in model.Constraints)
+            {
+                var size = c.Coefficients.Length;
+            }
 
+            return null;
         }
     }
 }
