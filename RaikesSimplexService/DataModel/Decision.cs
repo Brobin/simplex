fecc9f905c144e337ed52d760eb3064be6f75862
Brobin <tobinjbrown@gmail.com>
2014-11-13 22:13:07 -0600

    initial commit

---
+++ RaikesSimplexService/DataModel/Decision.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace RaikesSimplexService.DataModel
+{
+    class Decision
+    {
+        Object domain;
+        string inspection;
+        public Decision(Object domain, string inspection)
+        //create two decision variables
+        {
+            this.domain = domain;
+            this.inspection = inspection;
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Simplex
+
+LP Solver for Raik283h
+
+Tobin, Hanna (Banana), Maggie
