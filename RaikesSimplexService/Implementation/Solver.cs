441d62a18245b0c219efe7f77988b75c317da9b2
Brobin <tobinjbrown@gmail.com>
2014-11-16 23:10:44 -0600

    output rhs, lhs, and z row

--- RaikesSimplexService/Implementation/Solver.cs
+++ RaikesSimplexService/Implementation/Solver.cs
@@ -15,6 +15,18 @@ namespace RaikesSimplexService.DuckTheSystem
     {
         public Model model { get; set; }
         private int sVariables;
+        public double[,] rhs;
+        public double[,] lhs;
+        public double[] zRow;
+
+        public Solver(Model model)
+        {
+            this.model = model;
+            this.AddSlackSurplusVariables(model);
+            this.createRhs();
+            this.createLhs();
+            this.createZRow();
+        }
 
         public Solution Solve(Model model)
         {
@@ -35,35 +47,106 @@ namespace RaikesSimplexService.DuckTheSystem
                 {
                     equality[i] = c.Coefficients[i];
                 }
-                var s = 0;
-                switch (c.Relationship)
-                {
-                    case Relationship.GreaterThanOrEquals:
-                        if (c.Value != 0)
-                            s = -1;
-                        break;
-                    case Relationship.LessThanOrEquals:
-                        s = 1;
-                        break;
-                }
+                var s = this.getSValue(c.Relationship, c.Value);
                 var complete = false;
                 for (int j = size; j < equality.Length; j++)
                 {
-                    if(!complete && j >= size + sUsed)
+                    if (!complete && j >= size + sUsed)
                     {
                         equality[j] = s;
                         complete = true;
                         sUsed++;
                     }
-                    else
-                    {
-                        equality[j] = 0;
-                    }
                 }
-
                 c.Coefficients = equality;
             }
+            this.model = model;
             return model;
+        }
+
+        public double[] createZRow()
+        {
+            Goal g = this.model.Goal;
+            double[] zRow = new double[g.Coefficients.Length + sVariables];
+            for(int i = 0; i < g.Coefficients.Length; i++)
+            {
+                switch(this.model.GoalKind)
+                {
+                    case GoalKind.Maximize:
+                        zRow[i] = 0 - g.Coefficients[i];
+                        break;
+                    case GoalKind.Minimize:
+                        zRow[i] = g.Coefficients[i];
+                        break;
+                }
+            }
+            this.zRow = zRow;
+            return zRow;
+        }
+
+        public double[,] createLhs()
+        {
+            var c = this.model.Constraints;
+            int cons = c.Count;
+            double[,] lhs = new double[1, cons];
+            int size = c[0].Coefficients.Length;
+            for(int i = 0; i < cons; i++)
+            {
+                for(int j = 0; j < size; j++)
+                {
+                    var current = c[i].Coefficients[j];
+                    var found = false;
+                    if(current == 1 || current == -1)
+                    {
+                        found = true;
+                        int k = 0;
+                        while(found && k < cons)
+                        {
+                            var below = c[k].Coefficients[j];
+                            if(below != 0)
+                            {
+                                if(k != i)
+                                    found = false;
+                            }
+                            k++;
+                        }
+                    }
+                    if (found)
+                    {
+                        lhs[0,i] = j;
+                        break;
+                    }
+                }
+            }
+            this.lhs = lhs;
+            return lhs;
+        }
+
+        public double[,] createRhs()
+        {
+            Model m = this.model;
+            double[,] rhs = new double[1,m.Constraints.Count];
+            for (int i = 0; i < m.Constraints.Count; i++ )
+            {
+                rhs[0,i] = m.Constraints[i].Value;
+            }
+            this.rhs = rhs;
+            return rhs;
+        }
+
+        private int getSValue(Relationship r, double value)
+        {
+            var s = 0;
+            switch (r)
+            {
+                case Relationship.GreaterThanOrEquals:
+                    if (value != 0)
+                        return -1;
+                    break;
+                case Relationship.LessThanOrEquals:
+                    return 1;
+            }
+            return 0;
         }
 
         public Matrix[,] fillInMatrix(Model model)
